c367e0a825d78e6374588e8daaee546677f28bad
Kiahna Carmack <Kiahna.carmack@gmail.com>
2022-03-05 23:06:22 -0800

    updates routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const userRoute = require('./user-routes');
 const thoughtRoute = require('./thought-routes');
 
-// add prefix of `/users` to routes created in `user-routes.js`
 router.use('/users', userRoute);
 router.use('/thoughts', thoughtRoute);
 
