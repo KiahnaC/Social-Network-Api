a36efa72b14655643f033f92744bbdcf0bcd53b3
Kiahna Carmack <Kiahna.carmack@gmail.com>
2022-03-05 23:08:10 -0800

    completed routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const api = require('./api');
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', api);
+
+
+router.use((req, res) => {
+  res.status(404).send('<h1>404 Error!</h1>');
+});
+
+module.exports = router;
